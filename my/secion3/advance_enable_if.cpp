42437d71409a924145339436594cd2244de1323e
yoowonsuk <52575749+yoowonsuk@users.noreply.github.com>
2022-06-19 14:51:18 +0900

    Create advance_enable_if.cpp

---
+++ my/secion3/advance_enable_if.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <list>
+#include <iterator>
+#include <type_traits>
+#include <iostream>
+
+template<typename T>
+std::enable_if_t < std::is_same_v<std::random_access_iterator_tag, typename std::iterator_traits<T>::iterator_category>, void>
+xadvance(T& it, typename std::iterator_traits<T>::difference_type sz) {
+	it += sz;
+}
+
+template<typename T>
+std::enable_if_t < ! std::is_same_v<std::random_access_iterator_tag, typename std::iterator_traits<T>::iterator_category>, void>
+xadvance(T& it, typename std::iterator_traits<T>::difference_type sz) {
+	while (sz--) it++;
+}
+
+int main() {
+	std::vector<int> c = { 1, 2, 3, 4, 5, 6, 7, 8,9 ,10 };
+	//std::list<int> c = { 1, 2, 3, 4, 5, 6, 7, 8,9 ,10 };
+
+	auto p = std::begin(c);
+	xadvance(p, 5);
+
+	std::cout << *p << std::endl;
+}
