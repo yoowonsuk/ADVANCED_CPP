730b2680573a6e679050ac2dd01b5ea73e51f76b
yoowonsuk <52575749+yoowonsuk@users.noreply.github.com>
2022-06-19 18:16:21 +0900

    Update ref_view1.cpp

--- my/section5/ref_view1.cpp
+++ my/section5/ref_view1.cpp
@@ -9,6 +9,7 @@ int main() {
 	//std::vector<int>& r1 = v1;
 	//std::vector<int>& r2 = v2;
 
+	// std::reference_wrapper의 view 버전
 	std::ranges::ref_view r1 = v1; // 이동 가능한 참조
 	std::ranges::ref_view r2 = v2; // 내부적으로 포인터
 	r1 = r2; // 1. 복사 2. r1 참조가 바뀐다. // 참조자는 이동 불가능, 한 번 초기화 끝 => 복사다.
@@ -17,4 +18,6 @@ int main() {
 	std::cout << v2.size() << std::endl;// 5
 	std::cout << r1.size() << std::endl;// 5
 	std::cout << r2.size() << std::endl;// 5
+
+	// &r1.base() == &v1
 }
